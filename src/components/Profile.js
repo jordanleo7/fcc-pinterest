5309978c7095c1f059696a35704ebda6de150353
Jordan Johnson <jordanleo7@hotmail.com>
2018-05-08 10:01:42 -0500

    Profile nav-posts router
    to display created or saved or new posts by a user

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,6 +5,28 @@ import Masonry from 'react-masonry-component'
 
 class Profile extends React.Component {
 
+  constructor(props) {
+    super()
+    this.state = {
+      display: "createdposts"
+    }
+    this.displayCreatedPosts = this.displayCreatedPosts.bind(this);
+    this.displaySavedPosts = this.displaySavedPosts.bind(this);
+    this.displayNewPost = this.displayNewPost.bind(this);
+  }
+
+  displayCreatedPosts() {
+    this.setState({ display: "createdposts" })
+  }
+
+  displaySavedPosts() {
+    this.setState({ display: "savedposts" })
+  }
+
+  displayNewPost() {
+    this.setState({ display: "newpost" })
+  }
+
   Profile() {
     console.log(this.props.data)
     if (this.props.data.loading) return <div>Loading</div>
@@ -15,8 +37,23 @@ class Profile extends React.Component {
           <span className="profile--username">{this.props.data.user.username}</span>
           <img 
             src={this.props.data.user.photo} 
-            alt="profile picture"
+            alt="profile"
           />
+        </div>
+        <div className="profile--nav-posts">
+          <button onClick={this.displayCreatedPosts} className="profile--button">Created</button>
+          <button onClick={this.displaySavedPosts} className="profile--button">Saved</button>
+          <button onClick={this.displayNewPost} className="profile--button">New</button>
+        </div>
+        <div>
+
+          { this.state.display === "createdposts" 
+            ? <div>created posts</div>
+            : this.state.display === "savedposts"
+            ? <div>saved posts</div>
+            : <div>new post</div>
+          }
+
         </div>
       </div>
     )
